b827a27693d533070c214cde8924a986e221692a
Alexis Peves <alexpeves@gmail.com>
2022-09-12 12:25:46 -0400

    navbar and edit components made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import axios from 'axios'
+import Navbar from './components/Navbar'
+import Edit from './components/Edit'
+
 
 function App() {
   return (
     <div className="App">
       
       <h1>Drinks</h1>
+      <Navbar/>
+      <Edit/>
 
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../App.css'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>Drinks</a></li>
+            <li><a href='#'>Post</a></li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Navbar
