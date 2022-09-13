19c41dc90fa09e2429def7cb84479494da2d9e59
smeixner <sarameix22@gmail.com>
2022-09-13 10:18:49 -0400

    adding header to post

--- src/components/Post.js
+++ src/components/Post.js
@@ -16,6 +16,7 @@ const Post = (props) => {
     const [likeColor, setLikeColor] = useState(" gray ");
     const [likeFill, setLikeFill] = useState("evenodd");
     const [newComment, setNewComment] = useState('');
+    const [showComments, setShowComments] = useState(false);
 
     // Function to Handle Toggling Like Button
     const handleLikeToggle = () => {
@@ -91,6 +92,58 @@ const Post = (props) => {
     // Return HTML Elements
     return (
         <div className='post-container'>
+            <div className='post-header'>
+                <div className='post-location-container'>
+                    <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M16.2721 10.2721C16.2721 12.4813 14.4813 14.2721 12.2721 14.2721C10.063 14.2721 8.27214 12.4813 8.27214 10.2721C8.27214 8.06298 10.063 6.27212 12.2721 6.27212C14.4813 6.27212 16.2721 8.06298 16.2721 10.2721ZM14.2721 10.2721C14.2721 11.3767 13.3767 12.2721 12.2721 12.2721C11.1676 12.2721 10.2721 11.3767 10.2721 10.2721C10.2721 9.16755 11.1676 8.27212 12.2721 8.27212C13.3767 8.27212 14.2721 9.16755 14.2721 10.2721Z"
+                        fill=" gray "
+                    />
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5.79417 16.5183C2.19424 13.0909 2.05438 7.39409 5.48178 3.79417C8.90918 0.194243 14.6059 0.054383 18.2059 3.48178C21.8058 6.90918 21.9457 12.6059 18.5183 16.2059L12.3124 22.7241L5.79417 16.5183ZM17.0698 14.8268L12.243 19.8965L7.17324 15.0698C4.3733 12.404 4.26452 7.97318 6.93028 5.17324C9.59603 2.3733 14.0268 2.26452 16.8268 4.93028C19.6267 7.59603 19.7355 12.0268 17.0698 14.8268Z"
+                        fill=" gray "
+                    />
+                    </svg>
+                    <h5 className='location-p'>{props.drink.location}</h5>
+                </div>
+                <svg
+                className='clickable-svg'
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                >
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M5 15C6.65685 15 8 13.6569 8 12C8 10.3431 6.65685 9 5 9C3.34315 9 2 10.3431 2 12C2 13.6569 3.34315 15 5 15ZM5 13C5.55228 13 6 12.5523 6 12C6 11.4477 5.55228 11 5 11C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13Z"
+                    fill=" gray "
+                />
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15ZM12 13C12.5523 13 13 12.5523 13 12C13 11.4477 12.5523 11 12 11C11.4477 11 11 11.4477 11 12C11 12.5523 11.4477 13 12 13Z"
+                    fill=" gray "
+                />
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M22 12C22 13.6569 20.6569 15 19 15C17.3431 15 16 13.6569 16 12C16 10.3431 17.3431 9 19 9C20.6569 9 22 10.3431 22 12ZM20 12C20 12.5523 19.5523 13 19 13C18.4477 13 18 12.5523 18 12C18 11.4477 18.4477 11 19 11C19.5523 11 20 11.4477 20 12Z"
+                    fill=" gray "
+                />
+                </svg>
+            </div>
             <img className='post-image' src={props.drink.image} />
             <div className='post-info-container'>
                 <div className='likes-container'>
@@ -115,22 +168,26 @@ const Post = (props) => {
                 <h3>{props.drink.name}</h3>
                 <h4>Ingredients</h4>
                 <p>{props.drink.ingredients}</p>
-                <h4 className='comments-title'>Comments</h4>
+                <h4 className='comments-title' onClick={() => {setShowComments(!showComments)}}>Comments</h4>
                 {
-                    commentsArray.map((comment, i) => {
-                        return (
-                            <div className='comment-container' key={i}>
-                                <p>{comment}</p>
-                                <button className='delete-comment-button' value={comment} onClick={handleCommentDelete}>x</button>
-                            </div>
-                            
-                        )
-                    })
+                    showComments ?
+                        commentsArray.map((comment, i) => {
+                            return (
+                                <>
+                                    <div className='comment-container' key={i}>
+                                        <p>{comment}</p>
+                                        <button className='delete-comment-button' value={comment} onClick={handleCommentDelete}>x</button>
+                                    </div>
+                                    <form className='add-comment-form' onSubmit={handleCommentSubmit}>
+                                        <input type="text" value={newComment} onChange={handleNewComment} name="comment" placeholder='Type comment here...' />
+                                        <input className='submit-button' value='Add Comment' type="submit"/>
+                                    </form>
+                                </>
+                            )
+                        })
+                    :
+                        null
                 }
-                <form className='add-comment-form' onSubmit={handleCommentSubmit}>
-                    <input type="text" value={newComment} onChange={handleNewComment} name="comment" placeholder='Type comment here...' />
-                    <input className='submit-button' value='Add Comment' type="submit"/>
-                </form>
             </div>
         </div>
     )
