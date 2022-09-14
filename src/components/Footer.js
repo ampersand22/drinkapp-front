a63e8599e5d2c631e07004d07c0805528b7cb72c
smeixner <sarameix22@gmail.com>
2022-09-14 10:22:59 -0400

    adding footer at bottom of page

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+/////////////
+// IMPORTS //
+/////////////
+
+import { useState } from 'react';
+
+///////////////////
+// POST FUNCTION //
+///////////////////
+
+const Footer = () => {
+    // Return HTML Elements
+    return (
+        <footer>
+            <h5 className="footer-text">© 2022</h5>
+            <img src='/wastd-logo.png' />
+            <h5 className="footer-text">All Rights Reserved.</h5>
+        </footer>
+    )
+}
+
+/////////////
+// EXPORTS //
+/////////////
+
+export default Footer;
